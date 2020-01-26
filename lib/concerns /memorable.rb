f6cdfee0afd3c80c6f869146b3b865d1c4125e7c
Taryn Johnson <taryn.johnson919@gmail.com>
2020-01-26 02:48:31 +0000

    Automatically backed up by Learn

--- lib/concerns /memorable.rb
+++ lib/concerns /memorable.rb
@@ -1,13 +1,17 @@
 module Memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
 
-  def reset_all
-    
-  end 
+    def count
+      self.all.count
+    end
+  end
   
-  def count 
-    
-    
-  end 
-
-
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
 end
